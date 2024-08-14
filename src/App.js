7b48f4f430d8ad60dbda5832d5884d58e12465cb
sagar <sagarsharmatech00@gmail.com>
2024-08-14 12:10:59 +0530

    Input field testing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
 const image = `https://images.pexels.com/photos/27224216/pexels-photo-27224216/free-photo-of-agua-pura.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load`
   return (
-    <>
+    <div className='App'>
     <p>First React Test Case</p>
+    <input type='text' placeholder='Enter User Name' />
     <img title={"AI generated image"} src={image} />
-    </>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { act } from 'react';
 
 test("test first react app", ()=>{
-  render(<App/>);
+  act(() => {
+    render(<App />);
+  });
   const text = screen.getByText(/First React Test Case/i);
   const title = screen.getByTitle(/AI generated image/i);
   expect(text).toBeInTheDocument();
   expect(title).toBeInTheDocument();
+})
+
+test("Testing input box",()=>{
+  act(() => {
+    render(<App />);
+  });
+  let checkInput = screen.getByRole("textbox");
+  let checkInputPlaceHolder = screen.getByPlaceholderText("Enter User Name")
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceHolder).toBeInTheDocument();
 })
